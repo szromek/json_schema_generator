c9f7d71e1cf075c183259d11f5527f1406a733ad
Krzysztof Szromek <k.szromek@exlabs.pl>
2018-06-07 14:08:20 +0200

    progres

--- lib/json_schema_generator.rb
+++ lib/json_schema_generator.rb
@@ -2,17 +2,16 @@ require "json_schema_generator/core_ext/hash"
 
 module JsonSchemaGenerator
   def self.generate(object)
-    schema = { schema: 'http://json-schema.org/draft-04/schema#' }
+    schema = { 'schema': 'http://json-schema.org/draft-04/schema#' }
     if object.is_a?(Hash)
       schema.merge!(
-        type: 'object',
-        properties: dig_into_hash(object),
-        required: object.keys
+        'type': 'object',
+        'properties': dig_into_hash(object),
       )
     elsif object.is_a?(Array)
       schema.merge!(
-        type: 'array',
-        items: dig_into_array(object)
+        'type': 'array',
+        'items': dig_into_array(object)
       )
     else
       schema.merge!(type: value_type(object))
@@ -25,18 +24,17 @@ module JsonSchemaGenerator
     hash.each_pair do |key, value|
       properties[key] = if value.is_a?(Hash)
         {
-          type: 'object',
-          required: value.keys,
-          properties: dig_into_hash(value)
+          'type': 'object',
+          'properties': dig_into_hash(value)
         }
       elsif value.is_a?(Array)
         {
-          type: 'array',
-          items: dig_into_array(value)
+          'type': 'array',
+          'items': dig_into_array(value)
         }
       else
         {
-          type: value_type(value)
+          'type': value_type(value)
         }
       end
     end
@@ -50,17 +48,17 @@ module JsonSchemaGenerator
 
     if object.is_a?(Hash)
       {
-        type: 'object',
-        properties: dig_into_hash(object),
-        required: object.keys
+        'type': 'object',
+        'properties': dig_into_hash(object),
+        'required': object.keys
       }
     elsif object.is_a?(Array)
       {
-        type: 'array',
-        items: dig_into_array(object)
+        'type': 'array',
+        'items': dig_into_array(object)
       }
     else
-      { type: value_type(object) }
+      { 'type': value_type(object) }
     end
   end
 
